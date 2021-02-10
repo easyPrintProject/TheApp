c3a5e1cfaf113391b8d0fb0328f8e6acdd7c54f2
rereRaghad <78446332+rereRaghad@users.noreply.github.com>
2021-02-10 03:16:51 +0300

    Update AppStartScreen.tsx
    testimg

--- screens/AppStartScreen.tsx
+++ screens/AppStartScreen.tsx
@@ -17,7 +17,7 @@ export default function AppStartScreen() {
                     backgroundColor:"#00716F",
                     paddingVertical:10,
                     borderRadius:23, 
-                    margin: 7
+                    margin: 8
                 }}>
       <Button
   onPress={()=> alert("Login page")}
