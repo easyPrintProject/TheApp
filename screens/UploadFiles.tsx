f208c05f7a40d9b6d897a086112ae964f6f6815f
rawan <63139645+rawan321@users.noreply.github.com>
2021-02-12 07:13:42 +0300

    update2

--- screens/UploadFiles.tsx
+++ screens/UploadFiles.tsx
@@ -1,3 +1,32 @@
-import * as React from 'react';
-import { View, Button, TextInput, StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View,  Button } from 'react-native';
+import { DocumentPicker, ImagePicker } from 'expo';
 
+export default class UploadFiles extends React.Component {
+    state = {
+    };
+DocPick = async () => {
+	    let DocPrint = await DocumentPicker.getDocument({});
+		  alert(DocPrint.uri);
+      console.log(DocPrint);
+	}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button color='#8098db'
+          title=" حدد الملف" 
+          onPress={this.DocPick} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
