5866543e5998b2b3b53376032578706e88e0fca1
Rana98al <78611595+Rana98al@users.noreply.github.com>
2021-03-16 00:32:47 +0300

    تعديل على الصفحة الرئيسية و الحساب

--- screens/AccountScreen.tsx
+++ screens/AccountScreen.tsx
@@ -1,6 +1,33 @@
 import * as React from 'react';
+<<<<<<< Updated upstream
 import { StyleSheet, SafeAreaView  } from 'react-native';
 import { Text, View } from '../components/Themed';
+=======
+import { StyleSheet, SafeAreaView, Image, Text ,Alert, Modal, Pressable,Button, ImageBackground} from 'react-native';
+import FontAwesome from '@expo/vector-icons/build/FontAwesome';
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { View } from '../components/Themed';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {AccountParamList} from '../types'
+import { useGlobalState, GlobalStateInterface } from '../components/StateProvider';
+import { Types, UserType } from '../components/Reduser';
+import { StackScreenProps } from '@react-navigation/stack';
+
+
+export default function AccountScreen({navigation}: StackScreenProps<AccountParamList> ) {
+// ميثود للذهاب الى صفحة التعديل 
+const   GoToEditAccountScreen  = () => {
+
+  navigation.navigate('EditAccountScreen');
+}
+
+  const {state ,setState } = useGlobalState();
+  // const { state, dispatch } = useContext(AppContext);
+
+  React.useEffect(() => {
+     }, [])
+
+>>>>>>> Stashed changes
 
 export default function AccountScreen() {
   return (
@@ -8,6 +35,41 @@ export default function AccountScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>صفحة حساب المستخدم</Text>
     </View>
+<<<<<<< Updated upstream
+=======
+
+        <View style={styles.row}>
+          <Icon name="phone" color="#80CBC4 " size={20}/>
+          <Text style={styles.tt2}>{state.PhoneNumber}</Text>
+        </View>
+
+    </View>
+
+     {/* <View style={styles.infoBoxWrapper}>
+          <View style={[styles.infoBox,styles.cont3]}>
+            <Text>140.50</Text>
+            <Text>Wallet</Text>
+          </View>
+          
+      </View>  */}
+
+      <View style={styles.menuWrapper}>
+        <TouchableOpacity style={styles.menuItem}>
+            <Icon name="heart-outline" color="#FF6347" size={25}/>
+            <Text style={styles.menuItemText}>Your Favorites</Text>
+          </TouchableOpacity>
+         <TouchableOpacity style={styles.menuItem}>
+            <Icon name="calculater" color="#FF6347" size={25}/>
+            <Text style={styles.menuItemText}>Your Favorites</Text>
+          </TouchableOpacity>
+     </View>
+     <TouchableOpacity style={styles.button}
+     onPress={() => GoToEditAccountScreen()}>
+     <Text>عدل بياناتي</Text>
+     </TouchableOpacity>
+         
+          
+>>>>>>> Stashed changes
 </SafeAreaView>
   );
 }
@@ -21,5 +83,124 @@ const styles = StyleSheet.create({
     color:"red",
     fontSize: 20,
     fontWeight: 'bold',
+<<<<<<< Updated upstream
+=======
+  },
+
+  // button: {
+  //   padding: 20,
+  //   margin:20,
+  //   borderRadius: 30,
+  //  width:150,  
+  //  flexDirection: 'column',
+  //  paddingHorizontal:20}, 
+
+  userInfoSection: {
+    
+    alignItems: 'flex-end',
+    
+    paddingHorizontal: '5%',
+    marginBottom: '3%',
+    padding:'1%',
+    
+  },
+  
+  caption: {
+   
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: '500',
+  },
+
+  row: {
+    flexDirection: 'row-reverse',
+    marginBottom: '2%',
+  },
+
+  infoBoxWrapper: {
+
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+    borderTopColor: '#dddddd',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    height: 100,
+  },
+
+  infoBox: {
+    width: '50%',
+    
+  },
+  menuWrapper: {
+    alignContent:'space-around'
+  },
+
+  menuItem: {
+    flexDirection: 'row-reverse',
+    paddingVertical: '5%',
+    paddingHorizontal: '3%',
+    padding:'1%'
+
+  },
+
+  menuItemText: {
+
+    alignItems:'flex-end',
+    color: '#80CBC4',
+    marginLeft: 20,
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+
+  avatar: {
+    
+    width: 80,
+    height: 80,
+    borderRadius: 40, 
+    margin: 10,
+  },
+
+  cont:{
+    flexDirection: 'row-reverse',
+     marginTop: 15
+  },
+  cont3:{
+    
+    borderRightColor: '#dddddd',
+    borderRightWidth: 1
+  },
+  tt:{
+    
+    marginTop:'5%',
+    alignContent:'flex-end',
+    marginBottom: '15%',
+    fontSize:24,
+  },
+  cont2:{
+    
+    marginRight: 20,
+    alignItems: 'flex-end',
+
+  },
+  tt2:{
+    color:"#777777", 
+    marginRight: '5%',
+    fontSize:14,
+    
+  },
+  button:{
+    
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    shadowColor:"#e67a73",
+    borderRadius:8,
+
+	color:'#ffffff',
+
+>>>>>>> Stashed changes
   }
 });
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, Image, Button, SafeAreaView } from 'react-native';
 import { Text, View } from '../components/Themed';
+<<<<<<< Updated upstream
 export default function HomeScreen() {
   return (
     <SafeAreaView>
@@ -143,3 +144,153 @@ padding:20,
 }
 );
 
+=======
+ import { useGlobalState, GlobalStateInterface } from '../components/StateProvider';
+import { Types, UserType } from '../components/Reduser';
+export default function HomeScreen({navigation }: StackScreenProps<HomeParamList>) {
+
+ // const myContext = useContext(NewContext);
+ const {state ,setState } = useGlobalState();
+ // const { state, dispatch } = useContext(AppContext);
+
+ useEffect(() => {
+    }, [])
+
+
+
+ const CheckUser = () => {
+   if ( state.Email==null) {
+     return(
+     
+       <View style={styles.topcontrap}>
+       <TouchableOpacity  onPress={() => GoToLogin()} style={styles.button}>
+        <Text style={{color:"#E57373"}}>تسجيل</Text>
+       </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => GoToSignUp()} style={styles.button}>
+       <Text style={{color:"#E57373"}}>حساب جديد</Text>
+      </TouchableOpacity></View>);           
+   } 
+   else {
+     return(
+       <View style={styles.topcontrap}>
+      <TouchableOpacity  style={styles.button}>
+       <Text style={{color:"#E57373"}}> {state.UserName}مرحبا </Text>
+      </TouchableOpacity></View>); 
+   }  
+ }
+ const GoToLogin = () => {
+   navigation.navigate('LoginScreen');
+ };
+ const GoToSignUp = () => {
+   navigation.navigate('SigninScreen');
+ };
+ 
+ return (
+     <SafeAreaView >
+               
+              
+         <View style={styles.cont}> 
+         <View style={styles.topcont}>{CheckUser()}</View>
+              
+              <Text style={styles.title}>EaSY PRINT</Text>
+              
+
+              <View style={styles.menuWrapper}>
+              <TouchableOpacity style={styles.but}>
+               <Text style={styles.options}>طلب طباعة</Text>
+              </TouchableOpacity>
+            
+               <TouchableOpacity style={styles.but}>
+                <Text style={styles.options}>ملزماتي</Text>
+              </TouchableOpacity>
+
+             <TouchableOpacity style={styles.but}>
+              <Text style={styles.options}>تبرع</Text>
+            </TouchableOpacity>
+
+          </View> 
+          </View>
+
+   </SafeAreaView>
+ );
+}
+
+const styles = StyleSheet.create({
+
+  menuWrapper: {
+    alignContent:'space-around'
+  },
+
+ title: {
+   color:"black",
+   fontSize: 10,
+   fontWeight: 'bold', 
+ },
+
+ header:{
+   flexDirection:"row",
+   padding:6, 
+    marginTop:"2%",
+    alignSelf:"auto"
+
+ },
+
+ cont:{
+   
+  width: '100%',
+  height:'100%',
+  alignItems: "center",
+  
+
+ },
+ 
+
+ but:{
+  justifyContent:"center",
+  alignContent:'space-around',
+  flexDirection: 'row-reverse',
+    paddingVertical: '5%',
+    paddingHorizontal: '3%',
+    padding:'1%',
+    backgroundColor:"#F44336",
+    
+  
+   
+ },
+
+topcont: {
+     width:'100%',
+     
+     
+      },
+      
+topcontrap:{
+  height:'30%',
+  flexDirection:"row",
+  alignItems: "flex-end",
+  justifyContent: "flex-start",
+     backgroundColor:"#CE93D8",
+   
+},
+
+button:{
+  width:90,
+  height:25,
+  alignItems:"center",
+  backgroundColor:'#D81B60',
+  margin:"3%",
+    padding:'1%', 
+  
+},
+
+options:{
+ fontWeight: "bold",
+  color:"#FFFFFF", 
+  fontSize:16
+},
+
+
+
+});
+>>>>>>> Stashed changes
